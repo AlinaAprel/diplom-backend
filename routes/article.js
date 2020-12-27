16f88922c99da17cb472df783fc43e4c1ea7947e
Alina Aprel <alina.aprelh@mail.ru>
2020-12-27 17:49:51 +0300

    review

--- routes/article.js
+++ routes/article.js
@@ -15,10 +15,9 @@ routerArticles.post('/', celebrate({
     image: Joi.string().required().pattern(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/),
   }),
 }), createArticle);
-
 routerArticles.delete('/:articleId', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().hex().length(24),
+  body: Joi.object().keys({
+    id: Joi.string().hex().max(24),
   }),
 }), deleteArticle);
 
